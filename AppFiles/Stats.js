f4aa7a9cf0cac99f0d55351aca49f2e4b18cb511
Jason <jasoncheng224@gmail.com>
2021-02-28 07:02:23 -0800

    .

--- AppFiles/Stats.js
+++ AppFiles/Stats.js
@@ -1,18 +1,18 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import React, {useState} from "react";
 import {getData} from "../MagicCamera"
 
 export default function Stats() {
-    const [stats, setStats] = useState("");
+    const [stats, setStats] = useState({});
     (async () => {
         setStats(await getData("stats"));
     })();
 
     return (
         <View style={{ padding: 40 }}>
-        <Text style={{ fontSize: 24 }}>Squats: {this.stats[0]}</Text>
-        <Text style={{ fontSize: 24 }}>Jumping Jacks: {this.stats[1]}</Text>
-        <Text style={{ fontSize: 24 }}>Dumbbells: {this.stats[2]}</Text>
+        <Text style={{ fontSize: 24 }}>Squats: {stats[0]}</Text>
+        <Text style={{ fontSize: 24 }}>Jumping Jacks: {stats[1]}</Text>
+        <Text style={{ fontSize: 24 }}>Dumbbells: {stats[2]}</Text>
         <Text style={{ fontSize: 24 }}>Push ups: 0</Text>
         <Text style={{ fontSize: 24 }}>Sit ups: 0</Text>
         <Text style={{ fontSize: 24 }}>Planks: 0</Text>
