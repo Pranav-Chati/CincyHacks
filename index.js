26ae5cdd2a6317c433a6fdbc776dbe7ccd7ce59e
pranavchati <pchati2003@gmail.com>
2021-02-28 02:02:46 -0500

    Created food page

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { registerRootComponent } from 'expo';
 //import App from './App';
 import Home from './AppFiles/Home'
 import Login from './AppFiles/Login'
+import Food from './AppFiles/Food'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Home);
+registerRootComponent(Food);
